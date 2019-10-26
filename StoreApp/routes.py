0d33256ce7eab2770c048267efaac73ee9749fdd
Tyler <ttimms@kent.edu>
2019-10-26 12:57:00 -0400

    home page template created

--- StoreApp/routes.py
+++ StoreApp/routes.py
@@ -4,4 +4,8 @@ from flask import render_template
 @storeApp.route('/')
 @storeApp.route('/index')
 def index():
-  return render_template('home.html', title='Home')
+  products = ({"name": "Cappuccino", "price": "5.00", "image": "static/img/cappuccino.png"},
+              {"name": "Black Coffee", "price": "2.00", "image": "static/img/coffee.png"},
+              {"name": "Iced Coffee", "price": "3.00", "image": "static/img/iced_coffee.jpeg"} 
+            )
+  return render_template('home.html', title='Home', products=products)
